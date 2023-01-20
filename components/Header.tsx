3d26610a8bd2f40687075f43d6361f297c76b35b
verfasor <22955563+verfasor@users.noreply.github.com>
2023-01-20 15:15:06 +0800

    Update Header.tsx

--- components/Header.tsx
+++ components/Header.tsx
@@ -6,15 +6,12 @@ export default function Header() {
     <header className="flex justify-center items-center w-full mt-5 border-b-2 pb-7 sm:px-4 px-2">
       <Link href="/" className="flex space-x-3">
         <Image
-          alt="header text"
+          alt="AI Paragraph Rewriter"
           src="/writingIcon.png"
           className="sm:w-12 sm:h-12 w-8 h-8"
           width={32}
           height={32}
         />
-        <h1 className="sm:text-4xl text-2xl font-bold ml-2 tracking-tight">
-          rewrite.sh
-        </h1>
       </Link>
     </header>
   );
