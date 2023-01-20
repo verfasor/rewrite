baa13c4924c7abf8efe02ed57335ea45274aba91
verfasor <22955563+verfasor@users.noreply.github.com>
2023-01-20 15:00:01 +0800

    Create Header.tsx

--- components/Header.tsx
+++ components/Header.tsx
@@ -3,7 +3,7 @@ import Link from "next/link";
 
 export default function Header() {
   return (
-    <header className="flex justify-between items-center w-full mt-5 border-b-2 pb-7 sm:px-4 px-2">
+    <header className="flex justify-center items-center w-full mt-5 border-b-2 pb-7 sm:px-4 px-2">
       <Link href="/" className="flex space-x-3">
         <Image
           alt="header text"
@@ -16,19 +16,6 @@ export default function Header() {
           rewrite.sh
         </h1>
       </Link>
-      <a
-        href="https://studio.m64.in"
-        target="_blank"
-        rel="noreferrer"
-      >
-        <Image
-          alt="Vercel Icon"
-          src="/vercelLogo.png"
-          className="sm:w-8 sm:h-[27px] w-8 h-[28px]"
-          width={32}
-          height={28}
-        />
-      </a>
     </header>
   );
 }
